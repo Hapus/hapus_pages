b442b9e9bf6041a040ac7082785b1914d70649e4
Avadhut Phatarpekar <avadhut@stepout.com>
2012-06-04 17:52:37 +0530

    [#2] Added font list page

--- templates/font-list.tpl.php
+++ templates/font-list.tpl.php
@@ -1,9 +1,38 @@
-<?php 
-	// Populate the array only - do not display it
-    exec("convert -list font", $IMarray, $code);
+<?php
+	exec('/opt/local/bin/convert -list font', $output, $code);
 
-	print_r($IMarray);
-	print_r($code);
+	//Loop through and extract everything
+	if(!$code){
+		foreach($output as $font){
+			$font = explode(":", $font);
+			if(preg_match('/family/', $font[0])) {
+				$fontFamily[$font[1]] = $font[1];
+				$prev = $font[1];
+			}
+			if(preg_match('/style/', $font[0])) {
+				$styles[$prev][] = $font[1];
+			}
+		}
+	}
+
+	//Create the rows
+	foreach($fontFamily as $font){
+		$rows[] = array(
+			$font, 
+			implode(', ', array_unique($styles[$font]))
+		);
+	}
 ?>
-
-<h3 class="subTitle">Fonts list</h3>
+<h3 class="subTitle">Supported font families and styles</h3>
+<p>We currently support the following font families. However, if you do wish add a font of your own to this list, just <?php print l('drop us a line', 'add-font-form', array('attributes' => array('class' => 'colorbox-node'))) ?> and we will have it done for at no extra charges.</p>
+<?php
+	print theme_table(array(
+		'header' => array('Font family', 'Supported styles'),
+		'rows' => $rows,
+		'attributes' => array('width' => '100%'),
+		'sticky' => true,
+		'caption' => null,
+		'colgroups' => null,
+		'empty' => null
+	));	
+?>
