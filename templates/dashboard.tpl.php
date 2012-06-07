763e6e4844c959d9cd286ee2d5a947ac4dda450e
Avadhut Phatarpekar <avadhut@stepout.com>
2012-06-07 17:58:36 +0530

    Added the quickstarter block to the dashboard.

--- templates/dashboard.tpl.php
+++ templates/dashboard.tpl.php
@@ -2,6 +2,15 @@
 	//Include all the necessary JS and CSS files, some external
 	drupal_add_css(drupal_get_path('module', 'hapus_pages') . '/css/hapus_dashboard.css');
 
+	//Include all the necessary JS and CSS files, some external, for code sample generation
+	drupal_add_js('http://yandex.st/highlightjs/6.2/highlight.min.js', array('type' => 'external'));
+	drupal_add_css('http://yandex.st/highlightjs/6.2/styles/default.min.css', array('type' => 'external'));
+	drupal_add_js(drupal_get_path('module', 'hapus_pages') . '/js/api_examples.js');
+
+	//Empty dashboard options
+	$emptyList[] = 'Go through our ' . l('documentation', 'Documentation/API');
+	$emptyList[] = 'Mail us at ' . l('support@sprintpdf.com', 'mailto: support@sprintpdf.com') . '&mdash;we love answering questions :-)';
+
 	//Set the user and profile object
 	global $user;
 	$userProfile = profile2_load_by_user($user, 'main');
@@ -37,6 +46,14 @@
 		'<strong>Usage</strong>: ' . format_plural($fileUsage, '@count PDF', number_format($fileUsage) . ' PDFs') . ' generated',
 		'<strong>Remaining</strong>: ' . number_format($filesRemaining)
 	);
+
+	//Form the quick starter block
+	$blockList['quick_starter']['header'] = 'Quickstarter';
+	$blockList['quick_starter']['content'] = array(
+		'<span class="qsNumber">1</span>Download the most recent ' . l('Sprint PDF library', 'https://github.com/Hapus/SprintPDF_PHP_Library', array('external' => true, 'attributes' => array('target' => '_BLANK'))) . ' from Github.',
+		'<span class="qsNumber">2</span>Inlude it in your code ' . l('like this', 'api-example/generate-pdf/php', array('query' => array('height' => '500') ,'attributes' => array('class' => 'colorbox-node'))) . '.',
+		'<span class="qsNumber">3</span>Start PDFing!'
+	);	
 ?>
 
 <div id="dashboard">
@@ -53,7 +70,20 @@
 
 	<!--Right side content-->
 	<div id="dashRight">
-		<h2>Files generated this month</h2>
-		<?php print views_embed_view('user_file_usage') ?>
+		<?php if($fileUsage > 0): ?>
+			<h2>Files generated this month</h2>
+			<?php print views_embed_view('user_file_usage') ?>
+		<?php else: ?>
+			<h2>No PDFs generated this month</h2>
+			<p>If you need assistance with using Sprint PDF, you could:</p>
+			<?php 
+				print theme_item_list(array(
+					'items' => $emptyList,
+					'type' => 'ol',
+					'attributes' => array('id' => 'emptyHelp'),
+					'title' => null
+				));
+			?>
+		<?php endif; ?>
 	</div>
 </div>
